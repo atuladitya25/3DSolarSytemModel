915e562d390f1f69178425527db8d86bcdccbd9e
Atul <atuladitya2503@outlook.com>
2021-03-30 10:33:08 +0530

    Added orbital placeholders for Jupiter Venus and Mercury

--- 3dSolar.js
+++ 3dSolar.js
@@ -6,6 +6,7 @@ function main() {
 
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({canvas});
+    const gltfLoader = new GLTFLoader();
 
     const fov = 40;
     const aspect = 2;  // the canvas default
@@ -16,7 +17,7 @@ function main() {
     const controls = new OrbitControls(camera, canvas);
     controls.update();
 
-    camera.position.set(0, 0, 100);
+    camera.position.set(0, 0, 140);
     camera.up.set(0, 0, 1);
     camera.lookAt(0, 0, 0);
 
@@ -31,85 +32,173 @@ function main() {
     const sphereGeometry = new THREE.SphereGeometry(
         radius, widthSegments, heightSegments);
 
-    const solarSystem = new THREE.Object3D();
-    scene.add(solarSystem);
-    objects.push(solarSystem);
-
     //const textureLoader = new THREE.TextureLoader();
-
     //const glowMap = textureLoader.load("./resources/glow.png");
 
-    let sunMesh = new THREE.Object3D();
-    let earthLocale = new THREE.Object3D();
-    
-    const gltfLoader = new GLTFLoader();
-
-    gltfLoader.load('./resources/Sun_3D_Nasa.glb', (gltf) => {
-        sunMesh = gltf.scene;
-        sunMesh.scale.set(0.01, 0.01, 0.01); 
-        solarSystem.add(sunMesh);
-
-        // const spriteMaterial = new THREE.SpriteMaterial( 
-        //     { 
-        //         map: glowMap,
-        //         color: 0xfffa99, transparent: false, blending: THREE.AdditiveBlending
-        //     });
-        // const sprite = new THREE.Sprite( spriteMaterial );
-        // sprite.scale.set(100, 100, 100);
-        // solarSystem.add(sprite);
-    });
+    const solarSystem = new THREE.Object3D();
+    let sunModel = new THREE.Object3D();
 
     const earthOrbit = new THREE.Object3D();
-    solarSystem.add(earthOrbit);
-    objects.push(earthOrbit);
-
+    const earthLocale = new THREE.Object3D();
     let earthModel = new THREE.Object3D();
 
-    gltfLoader.load('./resources/Earth_3D_Nasa.glb', (gltf) => {
-        earthModel = gltf.scene;
-        earthModel.scale.set(0.0025, 0.0025, 0.0025);
-        earthLocale.add(earthModel);
-    });
-
-    earthLocale.position.z = 30;
-    earthOrbit.add(earthLocale);
-    objects.push(earthLocale);
-
     const moonOrbit = new THREE.Object3D();
-    earthLocale.add(moonOrbit);
-    
-    const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
-    const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-    moonMesh.position.z = 5;
-    moonMesh.scale.set(.5, .5, .5);
-    moonOrbit.add(moonMesh);
-    objects.push(moonMesh);
+    let moonModel = new THREE.Object3D();
 
     const marsOrbit = new THREE.Object3D();
-    solarSystem.add(marsOrbit);
-    objects.push(marsOrbit);
-
-    const marsMaterial = new THREE.MeshPhongMaterial({color: 0xE27B58, emissive: 0x112244});
-    const marsMesh = new THREE.Mesh(sphereGeometry, marsMaterial);
-    marsMesh.position.z = 50;
-    marsMesh.scale.set(.75, .75, .75);
-    marsOrbit.add(marsMesh);
-    objects.push(marsMesh);
+    const marsLocale = new THREE.Object3D();
+    let marsModel = new THREE.Object3D();
 
     const deimosOrbit = new THREE.Object3D();
-    marsMesh.add(deimosOrbit);
-    
-    const deimosMaterial = new THREE.MeshPhongMaterial({color: 0xE27B58, emissive: 0x222222});
-    const deimosMesh = new THREE.Mesh(sphereGeometry, deimosMaterial);
-    deimosMesh.position.z = 3;
-    deimosMesh.scale.set(.15, .15, .15);
-    deimosOrbit.add(deimosMesh);
-    objects.push(deimosMesh);
+    let deimosModel = new THREE.Object3D();
+
+    const venusOrbit = new THREE.Object3D();
+    const venusLocale = new THREE.Object3D();
+    let venusModel = new THREE.Object3D();
+
+    const mercuryOrbit = new THREE.Object3D();
+    const mercuryLocale = new THREE.Object3D();
+    let mercuryModel = new THREE.Object3D();
+
+    const jupiterOrbit = new THREE.Object3D();
+    const jupiterLocale = new THREE.Object3D();
+    let jupiterModel = new THREE.Object3D();
+
+    function addSunEnvironment() {
+
+        scene.add(solarSystem);
+        objects.push(solarSystem);
+
+        gltfLoader.load('./resources/Sun_3D_Nasa.glb', (gltf) => {
+            sunModel = gltf.scene;
+            sunModel.scale.set(0.015, 0.015, 0.015); 
+            solarSystem.add(sunModel);
+
+            // const spriteMaterial = new THREE.SpriteMaterial( 
+            //     { 
+            //         map: glowMap,
+            //         color: 0xfffa99, transparent: false, blending: THREE.AdditiveBlending
+            //     });
+            // const sprite = new THREE.Sprite( spriteMaterial );
+            // sprite.scale.set(100, 100, 100);
+            // solarSystem.add(sprite);
+        });
+    }
+
+    function addEarthEnvironment() {
+
+        solarSystem.add(earthOrbit);
+        objects.push(earthOrbit);
+
+        gltfLoader.load('./resources/Earth_3D_Nasa.glb', (gltf) => {
+            earthModel = gltf.scene;
+            earthModel.scale.set(0.0025, 0.0025, 0.0025);
+            earthLocale.add(earthModel);
+        });
+        
+        earthLocale.position.z = 50;
+        earthOrbit.add(earthLocale);
+        objects.push(earthLocale);
+
+        earthLocale.add(moonOrbit);
+        
+        const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
+        moonModel = new THREE.Mesh(sphereGeometry, moonMaterial);
+        moonModel.position.z = 5;
+        moonModel.scale.set(.5, .5, .5);
+        moonOrbit.add(moonModel);
+        objects.push(moonModel);
+
+    }
+
+    function addMarsEnvironment() {
+
+        solarSystem.add(marsOrbit);
+        objects.push(marsOrbit);
+
+        marsLocale.position.z = 70;
+        marsOrbit.add(marsLocale);
+        objects.push(marsLocale);
+
+        const marsMaterial = new THREE.MeshPhongMaterial({color: 0xE27B58, emissive: 0x112244});
+        marsModel = new THREE.Mesh(sphereGeometry, marsMaterial);
+        marsModel.scale.set(.75, .75, .75);
+        marsLocale.add(marsModel);
+        objects.push(marsModel);
+
+        marsLocale.add(deimosOrbit);
+        
+        const deimosMaterial = new THREE.MeshPhongMaterial({color: 0xE27B58, emissive: 0x222222});
+        deimosModel = new THREE.Mesh(sphereGeometry, deimosMaterial);
+        deimosModel.position.z = 3;
+        deimosModel.scale.set(.15, .15, .15);
+        deimosOrbit.add(deimosModel);
+        objects.push(deimosModel);
+
+    }
+
+    function addVenusEnvironment() {
+
+        solarSystem.add(venusOrbit);
+        objects.push(venusOrbit);
+
+        venusLocale.position.z = 35;
+        venusOrbit.add(venusLocale);
+        objects.push(venusLocale);
+
+        const venusMaterial = new THREE.MeshPhongMaterial({color: 0x00FF00, emissive: 0x112244});
+        venusModel = new THREE.Mesh(sphereGeometry, venusMaterial);
+        venusModel.scale.set(.8, .8, .8);
+        venusLocale.add(venusModel);
+        objects.push(venusModel);
+
+    }
+
+    function addMercuryEnvironment() {
+
+        solarSystem.add(mercuryOrbit);
+        objects.push(mercuryOrbit);
+
+        mercuryLocale.position.z = 20;
+        mercuryOrbit.add(mercuryLocale);
+        objects.push(mercuryLocale);
+
+        const mercuryMaterial = new THREE.MeshPhongMaterial({color: 0x0000FF, emissive: 0x112244});
+        mercuryModel = new THREE.Mesh(sphereGeometry, mercuryMaterial);
+        mercuryModel.scale.set(.5, .5, .5);
+        mercuryLocale.add(mercuryModel);
+        objects.push(mercuryModel);
+
+    }
+
+    function addJupiterEnvironment() {
+
+        solarSystem.add(jupiterOrbit);
+        objects.push(jupiterOrbit);
+
+        jupiterLocale.position.z = 120;
+        jupiterOrbit.add(jupiterLocale);
+        objects.push(jupiterLocale);
+
+        const jupiterMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000, emissive: 0x112244});
+        jupiterModel = new THREE.Mesh(sphereGeometry, jupiterMaterial);
+        jupiterModel.scale.set(2, 2, 2);
+        jupiterLocale.add(jupiterModel);
+        objects.push(jupiterModel);
+
+    }
 
     const color = 0xFFFFFF;
     const intensity = 8;
     const light = new THREE.PointLight(color, intensity);
     scene.add(light);
+
+    addSunEnvironment();
+    addEarthEnvironment();
+    addMarsEnvironment();
+    addVenusEnvironment();
+    addMercuryEnvironment();
+    addJupiterEnvironment();
 
     function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement;
@@ -132,17 +221,26 @@ function main() {
         }
 
         //solarSystem.rotation.x = -time/4;
-        sunMesh.rotation.x = -time;
+        sunModel.rotation.x = -time;
 
         earthOrbit.rotation.x = -time/4; 
-        earthModel.rotation.x = -time*2;
+        earthModel.rotation.x = -time*4;
 
         moonOrbit.rotation.x = -time*4;
 
         marsOrbit.rotation.x = -time/6;
-        marsMesh.rotation.x = -time*2;
+        marsModel.rotation.x = -time*2;
 
         deimosOrbit.rotation.x = -time*3;
+
+        venusOrbit.rotation.x = -time/3; 
+        venusModel.rotation.x = -time*4;
+
+        mercuryOrbit.rotation.x = -time/2;
+        mercuryModel.rotation.x = -time*6;
+
+        jupiterOrbit.rotation.x = -time/10;
+        jupiterModel.rotation.x = -time*6;
 
         renderer.render(scene, camera);
 
